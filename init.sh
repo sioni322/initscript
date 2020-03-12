b36421b0c95e02426f33a813a0e450f69eca7b81
sion <sioni322@naver.com>
2020-03-12 19:38:50 +0900

    initscript update:add nework setting

--- init.sh
+++ init.sh
@@ -4,16 +4,17 @@
 #sudo mv ./sources.list /etc/apt/
 #sudo apt-get update
 
-sudo apt-get install -y vim build-essential gcc make perl net-tools
+sudo apt-get install -y vim build-essential gcc make perl net-tools mdadm lvm2 openssh-server resolvconf
 
 sudo apt-get install -y ctags
 
 sudo apt install -y git
 
-sudo mv -f 01-network-manager-all.yaml /etc/netplan/
+sudo cp -f 01-network-manager-all.yaml /etc/netplan/
+sudp cp -f head /etc/resolvconf/resolv.conf.d/
 sudo netplan apply
 sudo systemctl restart networking
 
 sudo ufw enable
-
+sudp ufw allow 22/tcp
 
